c54e0b140bdd286f545aff2c0d499a068251ef0f
Fernando Augusto Rosa <134082690+FerdRosa@users.noreply.github.com>
2026-02-02 21:20:30 -0300

    Update Streamlit app configuration and logic
    Change -> 1.5 to 2.0

--- monitoria.py
+++ monitoria.py
@@ -6,7 +6,7 @@ import google.generativeai as genai
 API_KEY = st.secrets["GEMINI_KEY"] 
 
 genai.configure(api_key=API_KEY)
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-2.0-flash')
 
 # Configuração da Página
 st.set_page_config(page_title="Monitoria de Qualidade", layout="wide", page_icon="✅")
@@ -165,3 +165,4 @@ else:
     # Tela inicial vazia (instrução)
 
     st.info("👈 Preencha os dados na barra lateral para começar.")
+
